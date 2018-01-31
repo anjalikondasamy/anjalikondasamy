ed52d7155e489419597bc274960c8375b021a7bb
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-01-31 16:36:04 +0530

    good
    program

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ int n=100000;
+ if(a>0)
+ {
+ printf("\n Enter the positive number");
+ sacnf("%d",&a);
+ }
+ else(b<0)
+ {
+ printf("\n Enter the negative number");
+ sacnf("%d",&b);
+ }
+ else if(c=0)
+ {
+ printf("\n Enter the zero");
+ scanf("%d",&c);
+ }
+ }
